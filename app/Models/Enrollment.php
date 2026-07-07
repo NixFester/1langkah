2b1554906cd5380064deaa34c1c3b98211f848c7
NixFester <abi00kun@gmail.com>
2026-07-07 17:16:12 +0700

    fix enrollment, my fault of not using local test

--- app/Models/Enrollment.php
+++ app/Models/Enrollment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Services\AchievementService;
 use App\Services\XpService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
